4ef3f3adf6bbf6512d65d1a961e23ab0b087f3f8
sonetlumiere-code <lucas.perez.ok@gmail.com>
2023-07-10 17:48:36 -0300

    initial linting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task-manager-app'`, () => {
+  it('should have as title \'task-manager-app\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('task-manager-app');
